552cc3e6b3ce69502c346dc17baf11c5fb77a89d
CongQuy2001 <quylcph12604@fpt.edu>
2022-03-25 22:15:04 +0700

    first commit

---
+++ src/main/java/edu/poly/shop/conTroller/SimpleEmailExampleController.java
@@ -0,0 +1,35 @@
+package edu.poly.shop.conTroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.poly.shop.config.MyConstants;
+
+@Controller
+public class SimpleEmailExampleController {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @ResponseBody
+    @RequestMapping("/sendSimpleEmail")
+    public String sendSimpleEmail() {
+
+        // Create a Simple MailMessage.
+        SimpleMailMessage message = new SimpleMailMessage();
+        
+        message.setTo(MyConstants.FRIEND_EMAIL);
+        message.setSubject("Test Simple Email");
+        message.setText("Hello, Im testing Simple Email");
+
+        // Send Message!
+        this.emailSender.send(message);
+
+        return "Email Sent!";
+    }
+
+}
---
+++ src/main/java/edu/poly/shop/config/MyConstants.java
@@ -0,0 +1,14 @@
+package edu.poly.shop.config;
+
+public class MyConstants {
+
+    // Replace with your email here:  
+    public static final String MY_EMAIL = "lecongquy2401@gmail.com";
+
+    // Replace password!!
+    public static final String MY_PASSWORD = "123456789quy";
+
+    // And receiver!
+    public static final String FRIEND_EMAIL = "lecongquy2401@gmail.com";
+
+}
